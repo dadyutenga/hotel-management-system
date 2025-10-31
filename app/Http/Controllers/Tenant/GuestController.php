4dea55c890f9ec05202d16df093d4b27878186d6
Dady  Utenga <dadyutenga@yahoo.com>
2025-10-31 18:00:04 +0300

    bila   internet

--- app/Http/Controllers/Tenant/GuestController.php
+++ app/Http/Controllers/Tenant/GuestController.php
@@ -17,5 +17,18 @@ class GuestController extends Controller
             ->with('error', 'no  permission  ');
          }
 
+         $query =  Guest::with([]);
+         if($user->role->name== 'RECEPTIONIST'){
+            $query->whereHas('property', function($q) use ($user){
+                $q->where('tenant_id',$user->tenant_id);
+            });
+       } elseif ($user->role->name === 'MANAGER' && $user->property_id) {
+            $query->where('property_id', $user->property_id);
+       }   
+        elseif ($user->role->name === 'HOUSEKEEPER') {
+            // Housekeepers see only their assigned tasks
+            $query->where('assigned_to', $user->id);
+        }
+
     }
 }
