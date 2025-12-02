0969152a58790f56461ab875626ae4b3688d6094
Dadi Utenga <dadyutenga@yahoo.com>
2025-12-02 15:31:34 +0300

    fix: Update page title for consistency in branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to HotelPro - Hotel Management System</title>
+    <title>Welcome tmmo HotelPro - Hotel Management System</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
